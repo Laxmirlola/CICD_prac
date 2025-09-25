98d0ff04db805ac18ad104f4b176629d66a6064f
Laxmirlola Behera <laxmirlolabehera@gmail.com>
2025-09-25 13:56:45 +0530

    Add demo files

---
+++ jest.config.js
@@ -0,0 +1,4 @@
+export default {
+  textEnvironment: "node",
+  transform: {},
+};
---
+++ sum.test.js
@@ -0,0 +1,11 @@
+import sum from "./sum.js";
+
+describe("test for sum function", () => {
+  test("add 2+ 2 to equal to 4", () => {
+    expect(sum(2, 2)).toBe(4);
+  });
+
+  test("add -5+ -5 to equal to -10", () => {
+    expect(sum(-5, -5)).toBe(-10);
+  });
+});
